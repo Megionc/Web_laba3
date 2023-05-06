7422014ce353b14014256f0940d895ff765988d9
megionc <lyubchenko.kseniya@gmail.com>
2023-05-06 17:20:55 +0500

    Laba 3

---
+++ labapp/controllers/mainController.js
@@ -0,0 +1,22 @@
+exports.navmenu = [
+    {
+        name: 'Home',
+        addr: '/'
+    },
+    {
+        name: 'Story',
+        addr: '#'
+    },
+    {
+        name: 'Companies',
+        addr: '#'
+    },
+    {
+        name: 'Recordts',
+        addr: '#'
+    },
+    {
+        name: 'Review',
+        addr: '/review'
+    }
+];
